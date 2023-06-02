59b28f09c8e4e7f12b04271f3e7cafd4685cbf65
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2023-06-02 17:46:44 +0300

    Fix alg

--- alghoritms.py
+++ alghoritms.py
@@ -1,31 +1,12 @@
-quantity = int(input())
-climbers = []
+def subsequence(substring, whole_line):
+    position = -1
+    for i in substring:
+        position = whole_line.find(i, position + 1)
+        if position == - 1:
+            return False
+    return True
 
-for i in range(quantity):
-    in_ = [int(i) for i in input().split()]
-    climbers.append(in_)
+substring = input()
+whole_line = input()
 
-merged_climbers = []
-climbers = sorted(climbers, key=lambda x: x[0])
-
-for i in climbers:
-    if len(merged_climbers) == 0:
-        merged_climbers.append(i)
-    elif merged_climbers[-1][0] < i[0] and merged_climbers[-1][1] == i[0]:
-        merged_climbers[-1][1] = i[1]
-    elif merged_climbers[-1][0] < i[0] and merged_climbers[-1][1] > i[0] and i[1] > merged_climbers[-1][0] and i[1] > merged_climbers[-1][1]:
-        merged_climbers[-1][1] = i[1]
-    elif merged_climbers[-1][0] == i[0] and merged_climbers[-1][1] == i[1]:
-        continue
-    elif i[0] > merged_climbers[-1][0] and i[1]> merged_climbers[-1][1]:
-        merged_climbers.append(i)
-    elif merged_climbers[-1][0] < i[0] and merged_climbers[-1][1] > i[1]:
-        continue
-
-for i in merged_climbers:
-    print(*i)
-
-
-
-
-
+print(subsequence(substring, whole_line))
