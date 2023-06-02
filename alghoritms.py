f8085e2f9fe3a2054adb1b296edda0cbec109c39
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2023-06-02 16:43:54 +0300

    Fix alg

--- alghoritms.py
+++ alghoritms.py
@@ -1,28 +1,31 @@
 quantity = int(input())
 climbers = []
-step = quantity - 1
-mem_del = 0
 
 for i in range(quantity):
     in_ = [int(i) for i in input().split()]
     climbers.append(in_)
 
-print(climbers)j
+merged_climbers = []
+climbers = sorted(climbers, key=lambda x: x[0])
 
-for i in range(quantity):
-    for j in range(step):
-        if climbers[j][0] == climbers[j+1][0] and climbers[j][1] == climbers[j+1][1]:
-            mem = climbers[len(climbers)-1]
-            climbers[j], climbers[len(climbers)-1] = climbers[len(climbers)-1], climbers[j]
-            mem_del += 1
-        elif climbers[j][0] < climbers[j+1][0] and climbers[j][1] > climbers[j+1][1]:
-            climbers[j+1], climbers[len(climbers)-1-mem_del] = climbers[len(climbers)-1-mem_del], climbers[j+1]
-            mem_del += 1
-        elif climbers[j][0] > climbers[j+1][0] and climbers[j][1] < climbers[j+1][1]:
-            climbers[j], climbers[len(climbers)-1-mem_del] = climbers[len(climbers)-1-mem_del], climbers[j]
-            mem_del += 1
+for i in climbers:
+    if len(merged_climbers) == 0:
+        merged_climbers.append(i)
+    elif merged_climbers[-1][0] < i[0] and merged_climbers[-1][1] == i[0]:
+        merged_climbers[-1][1] = i[1]
+    elif merged_climbers[-1][0] < i[0] and merged_climbers[-1][1] > i[0] and i[1] > merged_climbers[-1][0] and i[1] > merged_climbers[-1][1]:
+        merged_climbers[-1][1] = i[1]
+    elif merged_climbers[-1][0] == i[0] and merged_climbers[-1][1] == i[1]:
+        continue
+    elif i[0] > merged_climbers[-1][0] and i[1]> merged_climbers[-1][1]:
+        merged_climbers.append(i)
+    elif merged_climbers[-1][0] < i[0] and merged_climbers[-1][1] > i[1]:
+        continue
 
-for i in range(mem_del):
-    climbers.pop()
-    
-print(climbers)
+for i in merged_climbers:
+    print(*i)
+
+
+
+
+
