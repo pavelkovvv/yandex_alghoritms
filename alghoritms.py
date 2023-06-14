9493545dcc02083af3766b60bbdbb7774d593a8d
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2023-06-14 11:00:56 +0300

    Add files

--- alghoritms.py
+++ alghoritms.py
@@ -1,10 +1,10 @@
-def get_mean(north, south, len_north, len_sou):
-    length = len_north + len_sou
+def get_mean(north, south, len_north, len_s):
+    length = len_north + len_s
     res_len = length // 2 + 1
     i, j = 0, 0
     res = []
     for k in range(res_len):
-        if i < len_north and j < len_sou:
+        if i < len_north and j < len_s:
             if north[i] < south[j]:
                 res.append(north[i])
                 i += 1
