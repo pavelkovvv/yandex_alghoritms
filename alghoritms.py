b459a628c79e37667df04780e5548612f3d8f5f6
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2023-06-17 07:33:39 +0300

    Fix alghoritms.py

--- alghoritms.py
+++ alghoritms.py
@@ -1,17 +1,17 @@
-def get_mean(north, south, len_nort, len_):
-    length = len_nort + len_
+def get_mean(north, south, len_nor, len_):
+    length = len_nor + len_
     res_len = length // 2 + 1
     i, j = 0, 0
     res = []
     for k in range(res_len):
-        if i < len_nort and j < len_:
+        if i < len_nor and j < len_:
             if north[i] < south[j]:
                 res.append(north[i])
                 i += 1
             else:
                 res.append(south[j])
                 j += 1
-        elif i < len_nort:
+        elif i < len_nor:
             res.append(north[i])
             i += 1
         else:
